f4c5ad623c974b91d90db6a8225af1aefe555e0b
Arturo Carrasco González <arturocarrascogonzalez@hotmail.com>
2024-10-28 14:03:38 +0100

    mountainCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Mountain from "./components/Mountain";
+import { db } from "./data/db";
 
 function App() {
-    return (
-      <>
+  const [data, setData] = useState(db);
 
-        <Header />        
-  
-        <main className="container-xl mt-5">
-          <h2 className="text-center">Nuestras Montañas</h2>
-  
-          <div className="row mt-5">
-              <Mountain />
-          </div>
-        </main>
-  
-        <footer className="bg-dark mt-5 py-5">
-          <div className="container-xl">
-              <p className="text-white text-center fs-4 mt-4 m-md-0">Peak Pathways Spain - Todos los derechos Reservados</p>
-          </div>
-        </footer>
-      </>
-    );
-  }
-  
-  export default App;
-  
+  return (
+    <>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestras Montañas</h2>
+
+        <div className="row mt-5 justify-content-center">
+          {data.map((mountain) => (
+            <Mountain
+              key={mountain.id} 
+              mountain={mountain}
+            />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            Peak Pathways Spain - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
--- src/components/Mountain.jsx
+++ src/components/Mountain.jsx
@@ -1,19 +1,21 @@
-export default function Mountain() {
+// eslint-disable-next-line react/prop-types
+export default function Mountain({ mountain }) {
+  // eslint-disable-next-line react/prop-types
+  const { name, image, description, price } = mountain;
+
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
       <div className="col-4">
         <img
           className="img-fluid"
-          src="./public/img/mountain_01.jpg"
+          src={`./public/img/${image}.jpg`}
           alt="imagen montaña"
         />
       </div>
       <div className="col-8">
-        <h3 className="text-black fs-4 fw-bold text-uppercase">Aneto</h3>
-        <p>
-        La montaña más alta de los Pirineos, ubicada en el Parque Natural Posets-Maladeta, famosa por su glaciar y vistas espectaculares. Altura: 3404 m
-        </p>
-        <p className="fw-black text-primary fs-3">250 €</p>
+        <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+        <p>{description}</p>
+        <p className="fw-black text-primary fs-3">{price} €</p>
         <button type="button" className="btn btn-dark w-100">
           Agregar al Carrito
         </button>
